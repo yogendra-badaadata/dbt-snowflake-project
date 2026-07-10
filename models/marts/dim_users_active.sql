f8a2bd455c4cb8d70dbfa33157e9b19a8ec9a111
yogendra-badaadata <yogendrafegade28@gmail.com>
2026-07-10 10:21:03 +0530

    test: rate limit commit #28

--- models/marts/dim_users_active.sql
+++ models/marts/dim_users_active.sql
@@ -1,1 +1,1 @@
-select id, first_name, status, user_role from {{ ref('raw_users') }} where status = 'active' and user_role = 'admin_27'
+select id, first_name, status, user_role from {{ ref('raw_users') }} where status = 'active' and user_role = 'admin_28'
