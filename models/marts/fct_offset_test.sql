a50b44deb2052c34d73e8a4832ba9fca9a125f78
yogendra-badaadata <yogendrafegade28@gmail.com>
2026-07-13 13:58:35 +0530

    Testing

--- models/marts/fct_offset_test.sql
+++ models/marts/fct_offset_test.sql
@@ -1,5 +1,5 @@
 select
     order_id,
     amount,
-    lag(amount, 1) over (order by order_date asc) as prev_amount
+    lag(amount, 2) over (order by order_date asc) as prev_amount
 from {{ ref('stg_orders') }}
