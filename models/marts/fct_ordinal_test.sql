cfa1fd3a79d66ea77ed8eabbdab05eb81e07b16f
yogendra-badaadata <yogendrafegade28@gmail.com>
2026-07-13 18:43:20 +0530

    testing

--- models/marts/fct_ordinal_test.sql
+++ models/marts/fct_ordinal_test.sql
@@ -1,6 +1,9 @@
 SELECT 
-    c.customer_id, 
+    c.cust_id, 
     SUM(o.amount) AS total_spend
-FROM customers c 
-JOIN orders o ON c.customer_id = o.customer_id
-GROUP BY c.customer_id
+FROM (
+    -- Renamed customer_id to cust_id inside subquery
+    SELECT customer_id AS cust_id FROM customers
+) c 
+JOIN orders o ON c.cust_id = o.customer_id
+GROUP BY c.cust_id
