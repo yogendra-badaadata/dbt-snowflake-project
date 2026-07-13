28840914696ff7aafbff9e7903095504ee63bf20
yogendra-badaadata <yogendrafegade28@gmail.com>
2026-07-13 17:28:41 +0530

    Testing

--- models/marts/fct_group_by_test.sql
+++ models/marts/fct_group_by_test.sql
@@ -1,11 +1,12 @@
 WITH base_orders AS (
+    -- Renamed user_id to usr_id inside the CTE
     SELECT 
-        user_id, 
+        user_id AS usr_id, 
         amount 
     FROM {{ ref('stg_orders') }}
 )
 SELECT
-    user_id,
+    usr_id,
     SUM(amount) AS total_amount
 FROM base_orders
-GROUP BY user_id
+GROUP BY usr_id
