152eaed4617200b3733ff95ceab3255a53b1a42c
yogendra-badaadata <yogendrafegade28@gmail.com>
2026-07-13 15:09:26 +0530

    Testing

--- models/marts/fct_logical_test.sql
+++ models/marts/fct_logical_test.sql
@@ -1,3 +1,3 @@
 select order_id, customer_id
 from {{ ref('stg_orders') }}
-where (customer_id = 1 or amount > 100 ) and status = 'completed'
+where (customer_id = 1 or amount > 200 ) and status = 'completed'
